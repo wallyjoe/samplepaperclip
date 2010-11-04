df4ac0eab52fbf28093ee55ca33fadde760c0a16
wally <wallyjoe8413@gmail.com>
2010-11-04 03:30:29 -0400

    Add style on paperclip

---
+++ app/models/user.rb
@@ -0,0 +1,4 @@
+class User < ActiveRecord::Base
+	has_attached_file :photo , :styles => { :medium => "300x300>", :thumb => "100x100>" }
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Testpaperclip::Application.config.secret_token = 'ec5df49ed4fafcbc8a71af500179e478cf2a1ab60b08b0071069b2e2531d82d72490a2a99a6e453713cebb3262716203719f7787ca7caa0a4914bd40d5ce28c2'
